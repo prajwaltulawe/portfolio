2a0c9e3f23ad7e7432730fffdcf1c7b1b05b3075
Prajwal Tulawe <tulaveprajwal143@gmail.com>
2023-07-05 18:06:53 +0530

    more left

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,18 @@
  } */
 
  function showProjects(){
-  var e = document.querySelectorAll(".d-none");
-  e.forEach(element => {
+  var hiddenProjects = document.querySelectorAll(".d-none");
+  hiddenProjects.forEach(element => {
       element.setAttribute('style', `display: flex`);
   });
- }
+  var viewMore = document.getElementsByClassName("view-more-button")[0];
+  viewMore.setAttribute('style', `display: none`);
+}
+
+function setActive(ref){
+  var otherNavItems = document.querySelectorAll(".nav-item");
+  otherNavItems.forEach(element => {
+      element.setAttribute('class', `nav-item`);
+  });
+  ref.parentElement.setAttribute('class', `nav-item active`)
+}
